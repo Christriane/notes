10c7a2a5bd93a9b87e2f2249dce8543788d27d25
Christriane <Christriane.marquez@gmail.com>
2018-07-02 16:43:29 +0000

    Select newly created note. Sort notes by updatedAt

--- imports/ui/NoteListHeader.test.js
+++ imports/ui/NoteListHeader.test.js
@@ -6,16 +6,40 @@ import Adapter from 'enzyme-adapter-react-16';
 import ReactRouterEnzymeContext from 'react-router-enzyme-context';
 
 import { NoteListHeader } from './NoteListHeader';
+import { notes } from '../fixtures/fixtures';
 
 if(Meteor.isClient){
     describe('NoteListHeader', function(){
+        let meteorCall;
+        let Session;
+        
+        beforeEach(function() {
+            meteorCall = expect.createSpy();
+            Session = {
+                set: expect.createSpy()
+            }
+        });
+        
         it('should call meteorCall on click', function(){
-            const spy = expect.createSpy();
             const options = new ReactRouterEnzymeContext();
-            const wrapper = mount(<NoteListHeader meteorCall={spy}/>, options.get());
+            const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session={Session}/>, options.get());
 
             wrapper.find('button').simulate('click');
-            expect(spy).toHaveBeenCalledWith('notes.insert');
+            meteorCall.calls[0].arguments[1](undefined, notes[0]._id);
+            
+            expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+            expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
+        });
+        
+        it('should not set session for failed insert', function(){
+            const options = new ReactRouterEnzymeContext();
+            const wrapper = mount(<NoteListHeader meteorCall={meteorCall} Session={Session}/>, options.get());
+
+            wrapper.find('button').simulate('click');
+            meteorCall.calls[0].arguments[1]({}, undefined);
+            
+            expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert');
+            expect(Session.set).toNotHaveBeenCalled();
         });
     });
 }
